801bdcfc9617bc79fb6b59fb48d745969143a07a
sundy-li <543950155@qq.com>
2018-07-12 10:34:28 +0800

    Fix url parser, database name matching bug

--- src/test/java/com/github/housepower/jdbc/ConnectionParamITest.java
+++ src/test/java/com/github/housepower/jdbc/ConnectionParamITest.java
@@ -1,11 +1,16 @@
 package com.github.housepower.jdbc;
 
+import com.github.housepower.jdbc.settings.ClickHouseConfig;
+import com.github.housepower.jdbc.settings.SettingKey;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class ConnectionParamITest {
 
@@ -15,5 +20,14 @@ public class ConnectionParamITest {
         Connection connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1?max_rows_to_read=1");
         Statement statement = connection.createStatement();
         statement.execute("SELECT 1 UNION ALL SELECT 2");
+    }
+
+    @Test
+    public void successfullyUrlParser() throws Exception {
+        Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
+        String url = "jdbc:clickhouse://127.0.0.1/system?min_insert_block_size_rows=1000";
+        ClickHouseConfig config = new ClickHouseConfig(url , new Properties());
+        Assert.assertEquals(config.database(), "system");
+        Assert.assertEquals(config.settings().get(SettingKey.min_insert_block_size_rows), 1000L);
     }
 }
