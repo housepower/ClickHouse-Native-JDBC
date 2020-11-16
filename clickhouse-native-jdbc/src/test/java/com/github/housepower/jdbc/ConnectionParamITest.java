fff44336545025f9362e48e9bf120a0bbffae75b
Cheng Pan <379377944@qq.com>
2020-11-16 17:18:47 +0800

    fix checkstyle

--- clickhouse-native-jdbc/src/test/java/com/github/housepower/jdbc/ConnectionParamITest.java
+++ clickhouse-native-jdbc/src/test/java/com/github/housepower/jdbc/ConnectionParamITest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
-import java.util.Enumeration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
