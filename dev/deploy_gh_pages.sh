83d329ba885258aa819c5ec8f152a972905160d6
Cheng Pan <379377944@qq.com>
2020-11-18 14:44:03 +0800

    update workflow

--- dev/deploy_gh_pages.sh
+++ dev/deploy_gh_pages.sh
@@ -6,5 +6,6 @@ git config --global user.name  'GitHub Workflow'
 git config --global user.email 'dummy@dummy.dummy'
 
 git init
-git commit -am 'Deploy GitHub Pages'
+git add -A
+git commit -m 'Deploy GitHub Pages'
 git push -f https://pan3793:${PAGES_DEPLOY_TOKEN}@github.com/housepower/ClickHouse-Native-JDBC.git master:gh-pages
