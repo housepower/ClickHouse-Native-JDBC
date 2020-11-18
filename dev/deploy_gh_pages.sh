605cc3a72fccacd4b3122195772de8ff4d6daa1f
Cheng Pan <379377944@qq.com>
2020-11-18 14:38:15 +0800

    update workflow

--- dev/deploy_gh_pages.sh
+++ dev/deploy_gh_pages.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
-
-npm run docs:build
-
 cd docs/.vuepress/dist
 
+git config --global user.name  'GitHub Workflow'
+git config --global user.email 'dummy@dummy.dummy'
+
 git init
-git add -A
-git commit -m 'Deploy GitHub Pages'
-git push -f git@github.com:powerhouse/ClickHouse-Native-JDBC.git master:gh-pages
+git commit -am 'Deploy GitHub Pages'
+git push -f https://pan3793:${PAGES_DEPLOY_TOKEN}@github.com/housepower/ClickHouse-Native-JDBC.git master:gh-pages
