d3f566563a6db8bc1f47724ab8e883014a9e5242
Cheng Pan <379377944@qq.com>
2020-11-20 07:07:45 +0800

    Fix DateTime64 nanos and scale (#217)
    * fix DateTime64 nanos and scale
    
    * code style
    
    * fix nano
    
    * IT

--- clickhouse-native-jdbc/src/main/java/com/github/housepower/jdbc/data/type/complex/DataTypeDateTime64.java
+++ clickhouse-native-jdbc/src/main/java/com/github/housepower/jdbc/data/type/complex/DataTypeDateTime64.java
@@ -24,6 +24,7 @@ import com.github.housepower.jdbc.serializer.BinaryDeserializer;
 import com.github.housepower.jdbc.serializer.BinarySerializer;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
@@ -35,19 +36,21 @@ public class DataTypeDateTime64 implements IDataType {
     public static DataTypeDateTime64 createDateTime64Type(SQLLexer lexer, ServerInfo serverInfo) throws SQLException {
         if (lexer.isCharacter('(')) {
             Validate.isTrue(lexer.character() == '(');
-            int precision = lexer.numberLiteral().intValue();
+            int scale = lexer.numberLiteral().intValue();
+            Validate.isTrue(scale >= MIN_SCALE && scale <= MAX_SCALA,
+                    "scale=" + scale + " out of range [" + MIN_SCALE + "," + MAX_SCALA + "]");
             if (lexer.isCharacter(',')) {
                 Validate.isTrue(lexer.character() == ',');
                 Validate.isTrue(lexer.isWhitespace());
                 String dataTimeZone = lexer.stringLiteral();
                 Validate.isTrue(lexer.character() == ')');
-                return new DataTypeDateTime64("DateTime64(" + precision + ", '" + dataTimeZone + "')", serverInfo);
+                return new DataTypeDateTime64("DateTime64(" + scale + ", '" + dataTimeZone + "')", scale, serverInfo);
             }
 
             Validate.isTrue(lexer.character() == ')');
-            return new DataTypeDateTime64("DateTime64(" + precision + ")", serverInfo);
+            return new DataTypeDateTime64("DateTime64(" + scale + ")", scale, serverInfo);
         }
-        return new DataTypeDateTime64("DateTime64", serverInfo);
+        return new DataTypeDateTime64("DateTime64", DEFAULT_SCALE, serverInfo);
     }
 
     // Since `Timestamp` is mutable, and `defaultValue()` will return ref instead of a copy for performance,
@@ -55,11 +58,18 @@ public class DataTypeDateTime64 implements IDataType {
     public static final Timestamp DEFAULT_VALUE = new Timestamp(0);
     public static final int NANOS_IN_SECOND = 1_000_000_000;
     public static final int MILLIS_IN_SECOND = 1000;
+    public static final int[] POW_10 = {1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000};
+    public static final int MIN_SCALE = 0;
+    public static final int MAX_SCALA = 9;
+    public static final int DEFAULT_SCALE = 3;
+
     private final String name;
+    private final int scale;
     private final ZoneId tz;
 
-    public DataTypeDateTime64(String name, ServerInfo serverInfo) {
+    public DataTypeDateTime64(String name, int scala, ServerInfo serverInfo) {
         this.name = name;
+        this.scale = scala;
         this.tz = DateTimeHelper.chooseTimeZone(serverInfo);
     }
 
@@ -90,12 +100,12 @@ public class DataTypeDateTime64 implements IDataType {
 
     @Override
     public int getPrecision() {
-        return 0;
+        return 20;
     }
 
     @Override
     public int getScale() {
-        return 19;
+        return scale;
     }
 
     @Override
@@ -114,19 +124,20 @@ public class DataTypeDateTime64 implements IDataType {
         Validate.isTrue(lexer.character() == ':');
         int minutes = lexer.numberLiteral().intValue();
         Validate.isTrue(lexer.character() == ':');
-        int seconds = lexer.intLiteral();
-        Validate.isTrue(lexer.character() == '.');
-        int nanos = lexer.intLiteral();
+        BigDecimal _seconds = BigDecimal.valueOf(lexer.numberLiteral().doubleValue())
+                .setScale(scale, BigDecimal.ROUND_HALF_UP);
+        int second = _seconds.intValue();
+        int nanos = _seconds.subtract(BigDecimal.valueOf(second)).movePointRight(9).intValue();
         Validate.isTrue(lexer.character() == '\'');
         Validate.isTrue(lexer.character() == ')');
 
-        ZonedDateTime zdt = ZonedDateTime.of(year, month, day, hours, minutes, seconds, nanos, tz);
+        ZonedDateTime zdt = ZonedDateTime.of(year, month, day, hours, minutes, second, nanos, tz);
         return Timestamp.from(zdt.toInstant());
     }
 
     @Override
     public Object deserializeBinary(BinaryDeserializer deserializer) throws IOException {
-        long value = deserializer.readLong();
+        long value = deserializer.readLong() * POW_10[MAX_SCALA - scale];
         long epochSeconds = value / NANOS_IN_SECOND;
         int nanos = (int) (value % NANOS_IN_SECOND);
         Timestamp timestamp = new Timestamp(epochSeconds * 1000);
@@ -135,13 +146,12 @@ public class DataTypeDateTime64 implements IDataType {
         return timestamp;
     }
 
-    //946674184456789
     @Override
     public void serializeBinary(Object data, BinarySerializer serializer) throws IOException {
         Timestamp timestamp = (Timestamp) data;
         long epochSeconds = timestamp.getTime() / MILLIS_IN_SECOND;
         int nanos = timestamp.getNanos();
-        long value = epochSeconds * NANOS_IN_SECOND + nanos;
+        long value = (epochSeconds * NANOS_IN_SECOND + nanos) / POW_10[MAX_SCALA - scale];
         serializer.writeLong(value);
     }
 
