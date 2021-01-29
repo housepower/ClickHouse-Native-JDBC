c6b4e3eb4bd278869be7f8308d5e37a96ec3ce3a
Neng Liu <neng.liu@kyligence.io>
2021-01-29 16:12:25 +0800

    retest

--- clickhouse-native-jdbc/src/main/java/com/github/housepower/data/type/complex/DataTypeNullable.java
+++ clickhouse-native-jdbc/src/main/java/com/github/housepower/data/type/complex/DataTypeNullable.java
@@ -143,6 +143,6 @@ public class DataTypeNullable implements IDataType {
 
     @Override
     public boolean isSigned() {
-        return nestedDataType.isSigned();
+        return nestedDataType.isSigned() ;
     }
 }
