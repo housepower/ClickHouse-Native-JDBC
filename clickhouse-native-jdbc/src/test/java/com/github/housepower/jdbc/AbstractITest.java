9e114fc060d184c91249cbcc7c2e70b106005196
Cheng Pan <379377944@qq.com>
2021-02-21 00:55:02 +0800

    (build) test agianst clickhouse-server:21.1 and enable grpc

--- clickhouse-native-jdbc/src/test/java/com/github/housepower/jdbc/AbstractITest.java
+++ clickhouse-native-jdbc/src/test/java/com/github/housepower/jdbc/AbstractITest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.ClickHouseContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
 
 import javax.sql.DataSource;
 import java.io.Serializable;
@@ -37,27 +38,36 @@ public abstract class AbstractITest implements Serializable {
     protected static final ZoneId SERVER_TZ = ZoneId.of("UTC");
     protected static final String DRIVER_CLASS_NAME = "com.github.housepower.jdbc.ClickHouseDriver";
 
-    public static final String CLICKHOUSE_IMAGE = SystemUtil.loadProp("CLICKHOUSE_IMAGE", "yandex/clickhouse-server:20.8");
+    // ClickHouse support gRPC from v21.1.2.15-stable 2021-01-18
+    // link: https://github.com/ClickHouse/ClickHouse/blob/master/CHANGELOG.md#clickhouse-release-v211215-stable-2021-01-18
+    public static final String CLICKHOUSE_IMAGE = SystemUtil.loadProp("CLICKHOUSE_IMAGE", "yandex/clickhouse-server:21.1");
 
     protected static final String CLICKHOUSE_USER = SystemUtil.loadProp("CLICKHOUSE_USER", "default");
     protected static final String CLICKHOUSE_PASSWORD = SystemUtil.loadProp("CLICKHOUSE_PASSWORD", "");
     protected static final String CLICKHOUSE_DB = SystemUtil.loadProp("CLICKHOUSE_DB", "");
 
+    protected static final int CLICKHOUSE_GRPC_PORT = 9100;
+
     @Container
     public static ClickHouseContainer container = (ClickHouseContainer) new ClickHouseContainer(CLICKHOUSE_IMAGE)
             .withEnv("CLICKHOUSE_USER", CLICKHOUSE_USER)
             .withEnv("CLICKHOUSE_PASSWORD", CLICKHOUSE_PASSWORD)
-            .withEnv("CLICKHOUSE_DB", CLICKHOUSE_DB);
+            .withEnv("CLICKHOUSE_DB", CLICKHOUSE_DB)
+            .withExposedPorts(CLICKHOUSE_GRPC_PORT)
+            .withCopyFileToContainer(MountableFile.forClasspathResource("grpc_config.xml"), "/etc/clickhouse-server/config.d/grpc_config.xml");
+
 
     protected static String CK_HOST;
     protected static String CK_IP;
     protected static int CK_PORT;
+    protected static int CK_GRPC_PORT;
 
     @BeforeAll
     public static void extractContainerInfo() {
         CK_HOST = container.getHost();
         CK_IP = container.getContainerIpAddress();
         CK_PORT = container.getMappedPort(ClickHouseContainer.NATIVE_PORT);
+        CK_GRPC_PORT = container.getMappedPort(CLICKHOUSE_GRPC_PORT);
     }
 
     /**
