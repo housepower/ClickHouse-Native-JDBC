5fedae14a643e1fb19096eed4d36ec5781c35bb0
ShengXiang <xs3316@126.com>
2021-05-28 12:43:13 +0800

    perf:solve the exception when insert array & map at same time

--- clickhouse-native-jdbc/src/main/java/com/github/housepower/data/ColumnFactory.java
+++ clickhouse-native-jdbc/src/main/java/com/github/housepower/data/ColumnFactory.java
@@ -22,14 +22,17 @@ import com.github.housepower.data.type.complex.DataTypeTuple;
 public class ColumnFactory {
 
     public static IColumn createColumn(String name, IDataType<?, ?> type, Object[] values) {
-        if (type instanceof DataTypeArray || type instanceof DataTypeMap)
-            return new ColumnArray(name, (DataTypeArray) ((DataTypeMap) type).getElementType(), values);
+        if (type instanceof DataTypeArray)
+            return new ColumnArray(name, (DataTypeArray) type, values);
 
         if (type instanceof DataTypeNullable)
             return new ColumnNullable(name, (DataTypeNullable) type, values);
 
         if (type instanceof DataTypeTuple)
             return new ColumnTuple(name, (DataTypeTuple) type, values);
+
+        if (type instanceof DataTypeMap)
+            return new ColumnArray(name, (DataTypeArray) ((DataTypeMap) type).getElementType(), values);
 
         return new Column(name, type, values);
     }
